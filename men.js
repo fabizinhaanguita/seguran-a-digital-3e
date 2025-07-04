162dee5a15412bdcee81d7ce11f65921de1a3d94
fabizinhaanguita <162513261+fabizinhaanguita@users.noreply.github.com>
2025-07-03 21:45:53 -0300

    ok

--- men.js
+++ men.js
@@ -1,4 +1,30 @@
-const numeroSenha = document.querySelector('.PARAMETRO-SENHA_TEXTO);
+const numeroSenha = document.querySelector('.parametro-senha_texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-const letrasMaiusculas = 
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusclas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('parametro-senha_botao');
+const campoSenha =document.querySelectorAll('.checkbox');
+const forcaSenhas = document.querySelector(foca);
+
+
+botoes[0].onclik = diminuiTamanho;
+botoes[1].onclik = diminuiTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha >1){
+        tamanhoSenha--;
+    }
+numeroSenha.textContent = tamanhoSenha;
+geraSenha();
+}
+    
+    function aumentaTamanho(){
+        if(tamanhoSenha < 20){
+            tamanhoSenha++;
+        }
+        numeroSenha.textContent = tamanhoSenha;
+        geraSenha();
+    }
